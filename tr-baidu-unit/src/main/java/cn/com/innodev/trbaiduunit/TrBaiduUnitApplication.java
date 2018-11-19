d6741311deb78eb40ef4722e308b8310376cc60a
yadong.zhang <yadong.zhang0415@gmail.com>
2018-11-19 16:17:02 +0800

    :tada: 初始化提交

---
+++ tr-baidu-unit/src/main/java/cn/com/innodev/trbaiduunit/TrBaiduUnitApplication.java
@@ -0,0 +1,12 @@
+package cn.com.innodev.trbaiduunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrBaiduUnitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrBaiduUnitApplication.class, args);
+    }
+}
---
+++ tr-baidu-unit/src/test/java/cn/com/innodev/trbaiduunit/TrBaiduUnitApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.innodev.trbaiduunit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrBaiduUnitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
